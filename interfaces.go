d1169dbc1bb383cbe6d4ef0fe54c6d0591ab45b8
wgeorge1990 <williameverettgeorge@icloud.com>
2019-09-23 18:08:12 -0400

    conversion and assertion

--- interfaces.go
+++ interfaces.go
@@ -6,7 +6,7 @@ import (
 
 type person struct {
 	first string
-	last string
+	last  string
 }
 
 type secretAgent struct {
@@ -28,8 +28,14 @@ type human interface {
 	speak()
 }
 
-func bar(h human){
-	fmt.Println("I called human")
+func bar(h human) {
+	switch h.(type) {
+	case person:
+		fmt.Println("this is a person")
+	case secretAgent:
+		fmt.Println("this is a secret agent")
+	}
+	fmt.Println("I called human", h)
 }
 
 func main() {
@@ -43,15 +49,21 @@ func main() {
 
 	p1 := person{
 		first: "doctor",
-		last: "yes",
+		last:  "yes",
 	}
 
 	sa.speak()
+	v := sa.person.first
+	fmt.Printf("%T\n", v)
+	 z := string(sa.person.first)
+	 fmt.Println(z)
+	 fmt.Printf("%T\n", z)
 	p1.speak()
 	fmt.Printf("%T\n", sa)
 	fmt.Println(p1)
 	bar(p1)
 	bar(sa)
+
 
 	//polymorphism allows method to take in multiple differnt types
 }
